cc93701e1593cdbb762e5a6a4f66c5c2af5b1f67
Nicolas <nicobava96@gmail.com>
2025-02-07 20:52:56 -0600

    Backend: booking integration testing working

--- api/src/rooming-list-booking/rooming-list-booking.module.ts
+++ api/src/rooming-list-booking/rooming-list-booking.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomingListBookingService } from './rooming-list-booking.service';
 import { RoomingListBookingController } from './rooming-list-booking.controller';
 import { RoomingListBooking } from './entities/rooming-list-booking.entity';
-import { RoomingList } from 'src/rooming-list/entities/rooming-list.entity';
-import { Booking } from 'src/booking/entities/booking.entity';
+import { RoomingList } from '../rooming-list/entities/rooming-list.entity';
+import { Booking } from '../booking/entities/booking.entity';
 
 @Module({
   imports: [
